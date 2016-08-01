b45540ad298728adaf2191f9b9232c0e6bf6cf47
Jovan <totika@live.com>
2016-08-01 23:52:41 +0200

    Created basic structure and made one month calendar

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,34 @@
-console.log("nesto o meni");
-console.log("nesto o meni 1");
-console.log("nesto o meni 2");
-console.log("n1 o meni 2");
-
 $(document).ready(function(){
-	$('h1').text('Tot breeee');
+	var h1 = $('<h1></h1>'),
+		currentMonth = moment().format('MMMM'),
+		currentYear = moment().format('Y'),
+		currentDay = moment().day();
+
+	$('main').append(h1.text(currentMonth+' '+currentYear));
+
+
+	for (var i = 1; i <= moment().daysInMonth(); i++) {
+			var div = $('<div class="singleDay"></div>');
+			$('.content').append(div.text(i));
+			if(i == currentDay){
+				$('.singleDay:nth-child(' + i + ')').addClass('current');
+			}
+	}
+
+	currentMonth = parseInt(moment().format('M'))-1;
+
+	$('.aRight').click(function(){
+		currentMonth+=1;
+		var nextMonth = moment().month(currentMonth).format('MMMM');
+		h1.text(nextMonth+' '+currentYear);
+	});
+
+	$('.aLeft').click(function(){
+		currentMonth-=1;
+		var nextMonth = moment().month(currentMonth).format('MMMM');
+		h1.text(nextMonth+' '+currentYear);
+	});
+
 });
+
+
