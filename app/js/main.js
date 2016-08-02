634b7835988b620c1a6755eae27f47a483d58055
Jovan <totika@live.com>
2016-08-02 23:57:32 +0200

    Fixed some issues with bower and infite browser sync

--- app/js/main.js
+++ app/js/main.js
@@ -1,150 +1,134 @@
-$(document).ready(function(){
-	var h1 = $('<h1></h1>'),
-		backToToday = $('<button>Back to Today</button>'),
-		currentMonth = moment().format('MMMM'),
-		currentYear = moment().format('Y'),
-		currentDay = moment().day(),		
-		currentDate = moment().format('MMMM')+' '+moment().format('Y');
+(function(){
+	$(document).ready(function(){
+		var h1 = $('<h1></h1>'),
+			backToToday = $('<button>Back to Today</button>'),
+			currentMonth = moment().format('MMMM'),
+			currentYear = moment().format('Y'),
+			currentDay = moment().day(),		
+			currentDate = moment().format('MMMM')+' '+moment().format('Y');
 
 
-	function init(){
-		$('.backTo').append(h1.text(currentDate));
-		$('.backTo').append(backToToday);
+		function init(){
+			$('.backTo').append(h1.text(currentDate));
+			$('.backTo').append(backToToday);
 
-	
-		for (var i = 1; i <= moment().daysInMonth(); i++) {
+		
+			for (var i = 1; i <= moment().daysInMonth(); i++) {
+					var div = $('<div class="singleDay"></div>');
+					$('.content').append(div.text(i));
+					if(i == currentDay){
+						$('.singleDay:nth-child(' + i + ')').addClass('current');
+					}
+			}
+		}
+		init();
+
+		function firstDayOfMonth(arg1,arg2,arg3){
+
+			function add(arg1,arg2,arg3){
+				return arg1+arg2+arg3;
+			}
+
+			var startOfMonthDay = moment(add(arg1,arg2,arg3)).startOf('month').format('llll').substr(0,3);
+			
+			switch(startOfMonthDay){
+				case 'Tue':
+					$('.singleDay:first-child').css({
+						'margin-left':'134px',
+					});
+				break;
+				case 'Wed':
+					$('.singleDay:first-child').css({
+						'margin-left':'264px',
+					});
+				break;
+				case 'Thu':
+					$('.singleDay:first-child').css({
+						'margin-left':'394px',
+					});
+				break;
+				case 'Fri':
+					$('.singleDay:first-child').css({
+						'margin-left':'524px',
+					});
+				break;
+				case 'Sat':
+					$('.singleDay:first-child').css({
+						'margin-left':'654px',
+					});
+				break;
+				case 'Sun':
+					$('.singleDay:first-child').css({
+						'margin-left':'784px',
+					});
+				break;
+			}
+		}
+		firstDayOfMonth();
+
+
+
+		currentMonth = parseInt(moment().format('M'))-1;
+		currentYear = parseInt(moment().format('Y'));
+
+		$('.aRight').click(function(){
+			currentMonth+=1;
+			var nextMonth = moment().month(currentMonth).format('MMMM');
+			if (nextMonth == 'January') {
+				currentYear+=1;
+				currentMonth=0;
+			}
+			h1.text(nextMonth+' '+currentYear);
+			$('.singleDay').remove();		
+
+			for(var i = 1; i <= moment(String(currentMonth+1)).daysInMonth(); i++){
 				var div = $('<div class="singleDay"></div>');
 				$('.content').append(div.text(i));
-				if(i == currentDay){
-					$('.singleDay:nth-child(' + i + ')').addClass('current');
+
+
+				if(h1.text() == currentDate){
+					if(i == currentDay){
+						$('.singleDay:nth-child(' + i + ')').addClass('current');
+					}
 				}
-		}
-	}
-	init();
-
-	function firstDayOfMonth(arg1,arg2,arg3){
-
-		function add(arg1,arg2,arg3){
-			return arg1+arg2+arg3;
-		}
-
-		var startOfMonthDay = moment(add(arg1,arg2,arg3)).startOf('month').format('llll').substr(0,3);
+			}
+				var newCurrentYear = String(currentYear),
+					sep = '/',
+					newCurrentMonth = String(currentMonth+1);
+				
+		firstDayOfMonth(newCurrentYear,sep,newCurrentMonth);
 		
-		switch(startOfMonthDay){
-			case 'Tue':
-				$('.singleDay:first-child').css({
-					'margin-left':'134px',
-				});
-			break;
-			case 'Wed':
-				$('.singleDay:first-child').css({
-					'margin-left':'264px',
-				});
-			break;
-			case 'Thu':
-				$('.singleDay:first-child').css({
-					'margin-left':'394px',
-				});
-			break;
-			case 'Fri':
-				$('.singleDay:first-child').css({
-					'margin-left':'524px',
-				});
-			break;
-			case 'Sat':
-				$('.singleDay:first-child').css({
-					'margin-left':'654px',
-				});
-			break;
-			case 'Sun':
-				$('.singleDay:first-child').css({
-					'margin-left':'784px',
-				});
-			break;
-		}
-	}
-	firstDayOfMonth();
+		});
 
 
+		$('.aLeft').click(function(){
+			currentMonth-=1;
+			var nextMonth = moment().month(currentMonth).format('MMMM');
+			if (nextMonth == 'December') {
+				currentYear-=1;
+				currentMonth=11;
+			}
+			h1.text(nextMonth+' '+currentYear);
+			$('.singleDay').remove();		
 
-	currentMonth = parseInt(moment().format('M'))-1;
-	currentYear = parseInt(moment().format('Y'));
+			for(var i = 1; i <= moment(String(currentMonth+1)).daysInMonth(); i++){
 
-	$('.aRight').click(function(){
-		currentMonth+=1;
-		var nextMonth = moment().month(currentMonth).format('MMMM');
-		if (nextMonth == 'January') {
-			currentYear+=1;
-			currentMonth=0;
-		}
-		h1.text(nextMonth+' '+currentYear);
-		$('.singleDay').remove();		
-
-		for(var i = 1; i <= moment(String(currentMonth+1)).daysInMonth(); i++){
-			var div = $('<div class="singleDay"></div>');
-			$('.content').append(div.text(i));
-
-
-			if(h1.text() == currentDate){
-				if(i == currentDay){
-					$('.singleDay:nth-child(' + i + ')').addClass('current');
+				var div = $('<div class="singleDay"></div>');
+				$('.content').append(div.text(i));
+				
+				if(h1.text() == currentDate){
+					if(i == currentDay){
+						$('.singleDay:nth-child(' + i + ')').addClass('current');
+					}
 				}
 			}
-		}
-			var newCurrentYear = String(currentYear),
-				sep = '/',
-				newCurrentMonth = String(currentMonth+1);
-			
-	firstDayOfMonth(newCurrentYear,sep,newCurrentMonth);
-	if(h1.text()!= currentDate){
-		$('button').fadeIn();
-	}else{
-		$('button').fadeOut();
-	}
-
-	
+		var newCurrentYear = String(currentYear),
+			sep = '/',
+			newCurrentMonth = String(currentMonth+1);
+		firstDayOfMonth(newCurrentYear,sep,newCurrentMonth);
+		});
 	});
+})();
 
-
-	$('.aLeft').click(function(){
-		currentMonth-=1;
-		var nextMonth = moment().month(currentMonth).format('MMMM');
-		if (nextMonth == 'December') {
-			currentYear-=1;
-			currentMonth=11;
-		}
-		h1.text(nextMonth+' '+currentYear);
-		$('.singleDay').remove();		
-
-		for(var i = 1; i <= moment(String(currentMonth+1)).daysInMonth(); i++){
-
-			var div = $('<div class="singleDay"></div>');
-			$('.content').append(div.text(i));
-			
-			if(h1.text() == currentDate){
-				if(i == currentDay){
-					$('.singleDay:nth-child(' + i + ')').addClass('current');
-				}
-			}
-		}
-	var newCurrentYear = String(currentYear),
-		sep = '/',
-		newCurrentMonth = String(currentMonth+1);
-	firstDayOfMonth(newCurrentYear,sep,newCurrentMonth);
-	if(h1.text()!= currentDate){
-		$('button').fadeIn();
-	}else{
-		$('button').fadeOut();
-	}
-	});
-
-
-	$('button').click(function(){		
-		$('.singleDay').remove();
-		currentYear = moment().format('Y');
-		$('button').fadeOut();
-		init();
-	})
-});
 
 
